c2614d3fc75997ad59ea8ed7ad0d291c9ce66faf
aahmedayman378-maker <aahmedayman378@gmail.com>
2026-09-01 15:54:01 +0300

    Add files via upload

---
+++ content.js
@@ -0,0 +1,34 @@
+
+// ============================================================
+//  EDIT THIS FILE to change the text and photos on the site.
+//  You don't need to touch style.css or script.js at all.
+// ============================================================
+
+const CONTENT = {
+  // The word(s) shown on the opening rain screen
+  introWords: ['HAPPY', 'BIRTHDAY'],
+
+  // Title & subtitle on the birthday/cake screen
+  occasionTitle: 'Happy Birthday Ya  Mony',
+  occasionSubtitle: 'a little gift, just for you',
+
+  // Each item below = one message + one or two photos that appear as you scroll.
+  // To add a photo: put the image filename in quotes, e.g. photos: ['photo1.jpg']
+  // (the image file needs to be in this same folder)
+  moments: [
+    { message: 'From the day I met you, everything felt different.', photos: ['photos/ph3.jpeg'] },
+    { message: 'Your laugh is my favorite sound in the world.', photos: ['photos/ph10.jpeg'] },
+    { message: "Every time I think of you, I can't help but smile.", photos: ['photos/ph1.jpeg'] },
+    { message: "No matter what happens, I'll always be by your side.", photos: ['photos/ph7.jpeg'] },
+  ],
+
+  // Message shown above the final heart-shaped photo collage
+  finalMessage: 'All our memories... in one heart.',
+
+  // Photos arranged into the heart shape at the end (up to 11 photos)
+ heartPhotos: ['photos/ph1.jpeg', 'photos/ph2.jpeg', 'photos/ph3.jpeg', 'photos/ph4.jpeg', 'photos/ph5.jpeg', 'photos/ph6.jpeg', 'photos/ph7.jpeg', 'photos/ph8.jpeg', 'photos/ph9.jpeg', 'photos/ph10.jpeg', ],
+ 
+
+  // Last line of the site
+  closingLine: 'Happy birthday to the best thing in my life 💕',
+};
